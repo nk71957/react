59e836e283380d4b38a7b3f8d64c7f994a19059a
nk71957 <nk71957@gmail.com>
2020-07-12 19:40:34 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+const fname= "Nishant";
+const lname= "Kumar";
+var d = new Date().toLocaleDateString();
+var t =new Date().toLocaleTimeString();
+const pic1 ="https://picsum.photos/200/300";
+const pic2 ="https://picsum.photos/250/300";
+const pic3 ="https://picsum.photos/270/300";
+const link ="https://google.com";
+ReactDOM.render(
+    <>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+        <h1 contentEditable="true">{`Welcome ${fname} ${lname}`}</h1>
+        <p>today date is : {d}</p>
+        <p>current time is : {t}</p>
+        <img src={pic1} alt="randomimage"/>
+        <img src={pic2} alt="randomimage"/>
+        <a href={link} target="_blank" rel="">
+            <img src={pic3} alt="randomimage"/>
+        </a>
+
+    </>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
